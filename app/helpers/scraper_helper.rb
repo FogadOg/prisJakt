1744ac3bb12f23d4971cd42f5e64fba739826608
FogadOg <fogadm06@gmail.com>
2024-02-03 18:32:50 +0100

    removing some irelevant code

--- app/helpers/scraper_helper.rb
+++ app/helpers/scraper_helper.rb
@@ -2,19 +2,4 @@ require 'nokogiri'
 require 'open-uri'
 
 module ScraperHelper
-    def findElements(url)
-        htmlContent = URI.open(url, 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-        
-        doc = Nokogiri::HTML(htmlContent)
-
-        elementClass = 'div._5qdMrS.w8MdNG.cYylcv.BaerYO._75qWlu.iOzucJ.JT3_zV._Qe9k6'
-
-        elements=doc.css(elementClass)
-        return elements 
-
-    end
-
-    def scrape(url)
-        puts findElements(url)
-    end
 end
