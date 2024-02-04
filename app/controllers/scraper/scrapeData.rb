6387cb8627b67e7862a7c73321776560d64716a0
FogadOg <fogadm06@gmail.com>
2024-02-04 06:17:54 +0100

    adding save method

--- app/controllers/scraper/scrapeData.rb
+++ app/controllers/scraper/scrapeData.rb
@@ -13,19 +13,33 @@ module Scraper
         end
 
         def getAttribute(attribute)
-            case attribute
-            when :link
-              return @link
-            when :image
-              return @image
-            when :name
-              return @name
-            when :price
-              return @price
-            else
-              raise ArgumentError, "Invalid attribute: #{attribute}"
-            end
+          case attribute
+          when :link
+            return @link
+          when :image
+            return @image
+          when :name
+            return @name
+          when :price
+            return @price
+          else
+            raise ArgumentError, "Invalid attribute: #{attribute}"
           end
+        end
+
+        def save
+          newProduct=Product.new(
+            link: link,
+            image: image,
+            name: name,
+            price: price
+          )
+          if newProduct.save
+            puts "Product created successfully!"
+          else
+            puts "Error creating product: #{newProduct.errors.full_messages.join(', ')}"
+          end
+        end
 
 
     end
