a69ba994e5939ab74bd3b4f3974088de57fd6f10
FogadOg <fogadm06@gmail.com>
2024-02-07 09:40:05 +0100

    adding cosine similarity method

--- app/controllers/search/tfIdfVector.rb
+++ app/controllers/search/tfIdfVector.rb
@@ -1,29 +1,59 @@
 module Search
     class TfIdfVector
-        def initialize(sentance)
-            @sentance=sentance
-            @tfIdf=TfIdf.new(sentance)
-
-            @vector=_createVector
+      def initialize()
+        @vector = []
+      end
+  
+      def generateTfIdfVector(tfIdfClass, sentence)
+        termList = sentence.split
+  
+        termList.each do |word| 
+          tfIdfScore = tfIdfClass.tfIdf(word)
+          @vector.append(tfIdfScore)
         end
+      end
+  
+      def cosineSimilarity(otherTfIdfVector)
+        raise "vectors must be same size" unless @vector.length == otherTfIdfVector.length
+  
+        dotProductA = _dotProduct(@vector, otherTfIdfVector.vector)
+        dotProductB = _dotProduct(otherTfIdfVector.vector, @vector)
+  
+        magnitudeA = _euclideanNorm(@vector)
+        magnitudeB = _euclideanNorm(otherTfIdfVector.vector)
 
-        def _createVector
-            vector=[]
-            words=@sentance.split
-            
-            words.each do |word|
-                tfIdf=@tfIdf.tfIdf(word)
-                vector.append(tfIdf)
-            end
-            return vector
+        similarity=(dotProductA * dotProductB) / (magnitudeA * magnitudeB)
+        if similarity==NaN
+            return 0
         end
-
-        def vector
-            @vector
+        return similarity
+      end
+  
+      def _dotProduct(vectorA, vectorB)
+        dotProduct = 0
+  
+        vectorA.each_with_index do |val, idx|
+          dotProduct += val * vectorB[idx]
         end
-
+  
+        return dotProduct
+      end
+  
+      def _euclideanNorm(vector)
+        return Math.sqrt(vector.map { |x| x**2 }.reduce(:+))
+      end
+  
+      def length
+        return @vector.length
+      end
+  
+      def addToVector(score)
+        @vector.append(score)
+      end
+  
+      def vector
+        @vector
+      end
     end
-end
-
-
-
+  end
+  
