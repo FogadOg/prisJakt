343721eb2018655c448062fc850f7dfa25cabe87
FogadOg <fogadm06@gmail.com>
2024-02-07 20:09:22 +0100

    adding search method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require_relative 'search/tfidf'
+
 class HomeController < ApplicationController
 
     def index
@@ -9,5 +11,35 @@ class HomeController < ApplicationController
         @product=Product.find(params[:id])
 
     end
+
+    def search
+        @query=params[:query]
+        @relatedProducts=[]
+        
+
+        textPreprocess=Search::TextProcess.new(@query)
+        processedQuery=textPreprocess.process
+        
+        queryTfIdf=Search::TfIdf.new(@query)
+        queryVector=Search::TfIdfVector.new()
+        queryVector.generateTfIdfVector(queryTfIdf, @query)
+
+        Product.all.each do |product|
+            textPreprocess=Search::TextProcess.new(product.name)
+            processedName=textPreprocess.process
+            
+            productTfIdf=Search::TfIdf.new(processedName)
+            prodcutVector=Search::TfIdfVector.new()
+            prodcutVector.generateTfIdfVector(productTfIdf, @query)
+
+            if prodcutVector.cosineSimilarity(queryVector)>0.08
+                @relatedProducts.append(product)
+            end
+        end
+
+        
+        
+    end
+
 
 end
