037550d338f8e4a4a4fe3f586eed8b958ce47f86
FogadOg <fogadm06@gmail.com>
2024-02-10 19:50:56 +0100

    adding functionaling for clustering of product providers

--- app/controllers/scraper/zalandoScraper.rb
+++ app/controllers/scraper/zalandoScraper.rb
@@ -8,13 +8,17 @@ module Scraper
         def scrape
 
             pageHtml=ScraperUtil.new(@doc)
+
+            logo=pageHtml.getLogo("link[rel='apple-touch-icon']")
+
             products=pageHtml.getProducts("._5qdMrS.w8MdNG.cYylcv.BaerYO._75qWlu.iOzucJ.JT3_zV._Qe9k6")
             
             products.save(
                 "._LM.JT3_zV.CKDt_l.CKDt_l.LyRfpJ",
                 ".sDq_FX.lystZ1.FxZV-M._2Pvyxl.JT3_zV.EKabf7.mo6ZnF._1RurXL.mo6ZnF._7ZONEy",               
                 ".sDq_FX.lystZ1.FxZV-M.HlZ_Tf.ZkIJC-.r9BRio.qXofat.EKabf7.nBq1-s._2MyPg2",
-                "p.sDq_FX.lystZ1.FxZV-M.HlZ_Tf",
+                "span.sDq_FX.lystZ1",
+                logo
                 
                 )
 
