be17fafd4329472b2dcc820e8ecf7fe528ff8325
FogadOg <fogadm06@gmail.com>
2024-02-11 10:12:25 +0100

    making search bar functional

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,29 +1,21 @@
 class SearchController < ApplicationController
-
     def search
-        @query=params[:query]
-
-        queryVector=TextProcessing::TfIdfVector.new().vectorForSearch(@query, @query)
-
-        @relatedProducts=getRelatedProducts(queryVector)
-        
+      @query = params[:query]
+      queryVector = TextProcessing::TfIdfVector.new().vectorForSearch(@query, @query)
+      @relatedProducts = getRelatedProducts(queryVector)
     end
-
+  
+    private
+  
     def getRelatedProducts(queryVector)
-        relatedProducts=[]
-
-        Product.all.each do |product|
-            
-            prodcutVector=TextProcessing::TfIdfVector.new().vectorForSearch(product.name, @query)
-
-            if prodcutVector.cosineSimilarity(queryVector)>0.01
-                relatedProducts.append(product)
-            end
-        end   
-
-        return relatedProducts
-
+      relatedProducts = []
+      Product.all.each do |product|
+        productVector = TextProcessing::TfIdfVector.new().vectorForSearch(product.name, @query)
+        if productVector.cosineSimilarity(queryVector) > 0.01
+          relatedProducts.append(product)
+        end
+      end
+      return relatedProducts
     end
-
-
-end
+  end
+  
