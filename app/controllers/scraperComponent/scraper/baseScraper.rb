cf273fbda2c53767aa8d0252c793f7e2120f233f
FogadOg <fogadm06@gmail.com>
2024-02-12 20:01:48 +0100

    removing the request method

--- app/controllers/scraperComponent/scraper/baseScraper.rb
+++ app/controllers/scraperComponent/scraper/baseScraper.rb
@@ -3,22 +3,12 @@ require 'open-uri'
 
 module ScraperComponent::Scraper
   class BaseScraper
-    def initialize(url, searchTerm, joiningSymbole)
-      @searchTerm = searchTerm
-      @joiningSymbole = joiningSymbole
-      @url = url
-
-      requestUrl = requestUrl()
-
+    def initialize(url, searchTerm)
+      requestUrl = url+searchTerm
+      
       htmlContent = URI.open(requestUrl, 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
       @doc = Nokogiri::HTML(htmlContent)
     end
 
-    def requestUrl
-      return @url if @searchTerm.nil?
-
-      searchTerm = @searchTerm.gsub(" ", @joiningSymbole)
-      @url + searchTerm
-    end
   end
 end
