b3a42b2133581bce52238c434882d73ccd404c9e
FogadOg <fogadm06@gmail.com>
2024-02-15 19:48:39 +0100

    new method for generating a vector for search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
     validates :name, presence: true
     validates :image, presence: true, allow_blank: false
+
+    def searchVector(otherTerm)
+
+        tfIdfVector=TextProcessing::TfIdfVector.new()
+        tfIdfVector=tfIdfVector.vectorForSearch(name, otherTerm)
+
+        return tfIdfVector
+    end
  
 end
