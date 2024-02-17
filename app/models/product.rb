3cc7e8ae1a18236ec31216b010337114091cf1b7
FogadOg <fogadm06@gmail.com>
2024-02-17 19:15:50 +0100

    new methods for calculating product price change

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,7 +17,45 @@ class Product < ApplicationRecord
             price: price, 
             image: image, 
             link: link
-            ).save
+        ).save
+
+        newPriceRecord(price)
+
+    end
+
+    def newPriceRecord(price)
+        PriceRecord.new(productId:id, price: price, date:Date.today).save
+    end
+
+    def extractDigits(string)
+        pattern = /\d[\d\s,.]*\d|\d/
+        matches = string.scan(pattern)
+        extracted_digits = matches.join('').gsub(',', '.')
+        extracted_digits.to_i
+
+    end
+
+    def priceChange
+        priceRecords=PriceRecord.where(productId: id)
+
+        oldestRecord = priceRecords.order(date: :asc).first
+        oldPrice=extractDigits(oldestRecord.price)
+
+        regex = /(?:\D*)(\d{1,3}(?:[., ]\d{3})*(?:,\d+)?)(?:\D*)/
+
+        sumPrice=0
+
+        priceRecords.each do |record|
+
+            sumPrice+=extractDigits(record.price)
+        end
+
+        avaragePrice=sumPrice/priceRecords.count
+        priceDifferance=(oldPrice-avaragePrice).to_f
+
+        differanceInPrecentage=(priceDifferance/avaragePrice)*100
+
+        return differanceInPrecentage.to_i
 
     end
 
