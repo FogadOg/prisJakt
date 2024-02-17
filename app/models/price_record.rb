0e929320f1691bf37992f45cbea7ac0feb94243b
FogadOg <fogadm06@gmail.com>
2024-02-17 11:19:27 +0100

    making fields mandetory

--- app/models/price_record.rb
+++ app/models/price_record.rb
@@ -1,2 +1,6 @@
 class PriceRecord < ApplicationRecord
+    validates :id, presence: true
+    validates :productId, presence: true
+    validates :price, presence: true
+    validates :date, presence: true
 end
