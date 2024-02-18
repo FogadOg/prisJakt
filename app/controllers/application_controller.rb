1d57dd82ff123c6f651d132137cce5bce895299e
FogadOg <fogadm06@gmail.com>
2024-02-18 15:02:12 +0100

    new methods for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    helper_method :currentUser
+
+    def currentUser
+      @currentUser ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    
+  def redirectLoggedInUser
+    if currentUser
+      redirect_to root_path 
+    end
+  end
 end
