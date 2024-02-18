944e46d558537c12fe153a17d31a4428b27bfd00
FogadOg <fogadm06@gmail.com>
2024-02-18 15:03:14 +0100

    adding redirect for loged in users

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,39 +1,40 @@
 class SignupController < ApplicationController
+  before_action :redirectLoggedInUser, only: [:new, :create]
     
-    def create
-        @user = User.new(user_params)
-        inputUserName = user_params["name"]
-        inputUserPassword = user_params["password"]
-  
-        if User.exists?(name: inputUserName)
-            @message = "user with the username already exists"
-            render :new, status: :unprocessable_entity
+  def create
+    @user = User.new(user_params)
+    inputUserName = user_params["name"]
+    inputUserPassword = user_params["password"]
 
-        elsif isPasswordValid(inputUserPassword)
-            @message = "invalid password"
-            render :new, status: :unprocessable_entity
-        else
-            @user.save
-            redirect_to root_path, notice: "You have been registered successfully."
-        end
-    end
+    if User.exists?(name: inputUserName)
+        @message = "user with the username already exists"
+        render :new, status: :unprocessable_entity
 
-    def isPasswordValid(password)
-        if password.present? && !password.match(/\A(?=.*[A-Z])(?=.*[0-9])(?=.*\W)/)
-          return true
-        end
-        return false
+    elsif isPasswordValid(inputUserPassword)
+        @message = "invalid password"
+        render :new, status: :unprocessable_entity
+    else
+        @user.save
+        redirect_to root_path, notice: "You have been registered successfully."
     end
+  end
 
-  
-    def new
-      @user = User.new
+  def isPasswordValid(password)
+    if password.present? && !password.match(/\A(?=.*[A-Z])(?=.*[0-9])(?=.*\W)/)
+      return true
     end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:name, :password)
-    end
+    return false
+  end
+
+
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
   end
   
