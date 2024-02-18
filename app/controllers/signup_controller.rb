d9c9e27e4aebf79d08cb8bdc712eda2aa20fa893
FogadOg <fogadm06@gmail.com>
2024-02-18 10:32:52 +0100

    making controller functional

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,39 @@
 class SignupController < ApplicationController
-end
+    
+    def create
+        @user = User.new(user_params)
+        inputUserName = user_params["name"]
+        inputUserPassword = user_params["password"]
+  
+        if User.exists?(name: inputUserName)
+            @message = "user with the username already exists"
+            render :new, status: :unprocessable_entity
+
+        elsif isPasswordValid(inputUserPassword)
+            @message = "invalid password"
+            render :new, status: :unprocessable_entity
+        else
+            @user.save
+            redirect_to root_path, notice: "You have been registered successfully."
+        end
+    end
+
+    def isPasswordValid(password)
+        if password.present? && !password.match(/\A(?=.*[A-Z])(?=.*[0-9])(?=.*\W)/)
+          return true
+        end
+        return false
+    end
+
+  
+    def new
+      @user = User.new
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :password)
+    end
+  end
+  
