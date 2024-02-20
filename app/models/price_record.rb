79ab8f187469a0a342f2c8dcecc3a26b01cbe1fb
FogadOg <fogadm06@gmail.com>
2024-02-20 14:41:04 +0100

    changing "productId" to "product_id"

--- app/models/price_record.rb
+++ app/models/price_record.rb
@@ -1,5 +1,5 @@
 class PriceRecord < ApplicationRecord
-    validates :productId, presence: true
+    validates :product_id, presence: true
     validates :price, presence: true
     validates :date, presence: true
     belongs_to :product
