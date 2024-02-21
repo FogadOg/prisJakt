5f7114107457d2effe6104f416394718e5c6e3d5
red cube <Farouqjalabi@gmail.com>
2024-02-21 19:11:07 +0100

    using devise to handle user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :installs
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,13 +10,6 @@ Rails.application.routes.draw do
 
   get "search", to: "search#search", as: "search"
   get "category", to: "category_page#category", as: "category"
-
-  get '/login', to: 'login#new'
-  delete '/logout', to: 'login#destroy'
-  post '/login', to: 'login#create'
-
-  get '/signup', to: 'signup#new'
-  post '/signup', to: 'signup#create'
 
   resources :custom_products, only: [:new, :create]
   resources :products, only: [:index, :show]
