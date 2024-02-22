cd439af4c3194d052459f3e8bd6478a6b04e1722
FogadOg <fogadm06@gmail.com>
2024-02-22 18:21:07 +0100

    fixing error

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,3 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -8,5 +6,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Importing chart for showing price history
 pin "chartkick", to: "chartkick.js"
-pin "Chart.bundle", to: "Chart.bundle.js"pin "@rails/actioncable", to: "actioncable.esm.js"
+pin "Chart.bundle", to: "Chart.bundle.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
