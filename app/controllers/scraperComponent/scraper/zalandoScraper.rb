321cfe96ac4ae96dea6e8ad2d1f55ff64b127fd5
FogadOg <fogadm06@gmail.com>
2024-02-26 20:26:18 +0100

    using new model

--- app/controllers/scraperComponent/scraper/zalandoScraper.rb
+++ app/controllers/scraperComponent/scraper/zalandoScraper.rb
@@ -13,14 +13,12 @@ module ScraperComponent::Scraper
 
             products=pageHtml.getProducts("._5qdMrS.w8MdNG.cYylcv.BaerYO._75qWlu.iOzucJ.JT3_zV._Qe9k6")
             
-            products.save(
+            ScraperComponent::ProcessProducts.new(products.element, logo).save(
                 "._LM.JT3_zV.CKDt_l.CKDt_l.LyRfpJ",
                 ".sDq_FX.lystZ1.FxZV-M._2Pvyxl.JT3_zV.EKabf7.mo6ZnF._1RurXL.mo6ZnF._7ZONEy",               
                 ".sDq_FX.lystZ1.FxZV-M.HlZ_Tf.ZkIJC-.r9BRio.qXofat.EKabf7.nBq1-s._2MyPg2",
                 "span.sDq_FX.lystZ1",
-                logo,
-                "clothes"
-                )
+                logo,)
 
         end
         
