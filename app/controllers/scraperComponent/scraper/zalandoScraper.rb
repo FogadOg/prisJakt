70580b43846e4318b5fd67ea7b52aeb04618f19b
FogadOg <fogadm06@gmail.com>
2024-02-27 19:19:18 +0100

    refactoring code

--- app/controllers/scraperComponent/scraper/zalandoScraper.rb
+++ app/controllers/scraperComponent/scraper/zalandoScraper.rb
@@ -1,7 +1,7 @@
 module ScraperComponent::Scraper
     class ZalandoScraper < BaseScraper
-        def initialize(url, searchTerm)
-            super(url, searchTerm)
+        def initialize(url, searchTerm, batchId)
+            super(url, searchTerm, batchId)
             scrape()
         end
         
@@ -13,12 +13,11 @@ module ScraperComponent::Scraper
 
             products=pageHtml.getProducts("._5qdMrS.w8MdNG.cYylcv.BaerYO._75qWlu.iOzucJ.JT3_zV._Qe9k6")
             
-            ScraperComponent::ProcessProducts.new(products.element, logo).save(
+            ScraperComponent::ProcessProducts.new(products.element, logo, @batchId).save(
                 "._LM.JT3_zV.CKDt_l.CKDt_l.LyRfpJ",
                 ".sDq_FX.lystZ1.FxZV-M._2Pvyxl.JT3_zV.EKabf7.mo6ZnF._1RurXL.mo6ZnF._7ZONEy",               
                 ".sDq_FX.lystZ1.FxZV-M.HlZ_Tf.ZkIJC-.r9BRio.qXofat.EKabf7.nBq1-s._2MyPg2",
-                "span.sDq_FX.lystZ1",
-                logo,)
+                "span.sDq_FX.lystZ1")
 
         end
         
