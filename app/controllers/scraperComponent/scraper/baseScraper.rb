ce97a34337c74a0be8bcbd6a03d9a9f4de3ab5a7
FogadOg <fogadm06@gmail.com>
2024-02-27 18:51:25 +0100

    new field for batchId

--- app/controllers/scraperComponent/scraper/baseScraper.rb
+++ app/controllers/scraperComponent/scraper/baseScraper.rb
@@ -3,7 +3,8 @@ require 'open-uri'
 
 module ScraperComponent::Scraper
   class BaseScraper
-    def initialize(url, searchTerm)
+    def initialize(url, searchTerm, batchId)
+      @batchId = batchId
       requestUrl = url+searchTerm
       
       htmlContent = URI.open(requestUrl, 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
