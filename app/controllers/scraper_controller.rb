81178e5b67e927cc87927fc991840e2c0acd33e2
red cube <Farouqjalabi@gmail.com>
2024-02-29 21:18:23 +0100

    All scrapers as service

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -1,28 +1,10 @@
-require_relative '../services/zalando_scraper'
+require_relative '../services/scrapers'
 
 class ScraperController < ApplicationController
     include ScraperHelper
     def index
 
-        scrape_record = ScrapeRecord.new(date:Date.today)
-        scrape_record.save
-
-        scrape_record_id=scrape_record.id.to_i
-       
-        searchTerms=[
-            "air jordan 1"
-        ]
-        searchTerms.each do |searchTerm|
-            ScraperComponent::Scraper::ZalandoScraper.new("https://www.zalando.no/herre/?q=", searchTerm ,scrape_record_id)
-            ScraperComponent::Scraper::NikeScraper.new("https://www.nike.com/no/w?q=",searchTerm, scrape_record_id)
-
-        end
-
-        new_price_record_for_all_sources(scrape_record_id)
-        
-        # Product.all.each do |product|
-        #     product.newPriceRecord(product.price)
-        # end
+        ScrapersService.new.call
 
         render json:{status:"200 success"}
     end
