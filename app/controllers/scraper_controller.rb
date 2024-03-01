1e014519b05f989de76beb46bcec277aad9e3d84
FarouqJalabi <farouqjalabi@gmail.com>
2024-03-01 17:48:13 +0100

    Made search_term code more readable

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -3,13 +3,16 @@ require_relative '../services/scrapers'
 class ScraperController < ApplicationController
     include ScraperHelper
     def index
-        if params[:search_params] and params[:search_params].split(",") != []
-            ScrapersService.new.call(search_terms: params[:search_params].split(","))
-            render json:{status:"200 success", searching_params: params[:search_params].split(",")}
-        else
-            ScrapersService.new.call
-            render json:{status:"200 success", searching_params: "air jordan 1"}
+        # separating search_terms by ","
+        search_terms = params[:search_terms]&.split(",")
+
+        # Defaults to air jordan 1
+        if not search_terms.present? or search_terms.empty?
+          search_terms = ["air jordan 1"]
         end
-        # ScrapersService.new.call
+
+        # TODO: Get amount of products and pricerecords made?
+        ScrapersService.new.call(search_terms: search_terms)
+        render json:{status:"200 success", searching_terms: search_terms}
     end
 end
