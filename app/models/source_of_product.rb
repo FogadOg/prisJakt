2fe62818f3e3fc28e0a8b573a5ec68fc5e99f477
FogadOg <fogadm06@gmail.com>
2024-03-01 19:18:23 +0100

    moved methods to source of product

--- app/models/source_of_product.rb
+++ app/models/source_of_product.rb
@@ -1,4 +1,28 @@
 class SourceOfProduct < ApplicationRecord
   belongs_to :product
   validates_associated :product
+
+  def newPriceRecord(batch_id, currency=nil)
+    PriceRecord.new(
+      product_id:id, 
+      price: 500, 
+      date:Date.today, 
+      currency: "currency",
+      batch: batch_id
+    ).save   
+
+    
+  end
+
+  def processPrice(batch_id)
+    # priceNumerical, currency = extractPriceAndCurrancy(price)
+        
+    PriceRecord.new(
+      product_id:id, 
+      price: 400, 
+      date:Date.today, 
+      currency: "currency",
+      batch: batch_id
+    ).save
+  end
 end
