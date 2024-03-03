fa31d1368a71896e84dde4fcc2e3a2b5d5e27225
FogadOg <fogadm06@gmail.com>
2024-03-03 21:47:38 +0100

    new price and currency extractor

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,20 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  def extractPriceAndCurrancy(string)
+    currency_pattern = /(\$|€|£|kr)/
+    price_pattern = /\d+(\.\d+)?/
+
+    currency_match = string.match(currency_pattern)
+    price_match = string.match(price_pattern)
+  
+    if currency_match && price_match
+      currency = currency_match[0]
+      price = price_match[0].to_i
+      return price, currency
+    else
+      return nil, nil
+    end
+
+
+  end
 end
