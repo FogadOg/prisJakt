d46fe5a6328f4a189be371281b67c8441dcbd168
FogadOg <fogadm06@gmail.com>
2024-03-04 20:59:06 +0100

    fine tuning

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,15 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-  def extractPriceAndCurrancy(string)
-    currency_pattern = /(\$|€|£|kr)/
-    price_pattern = /\d+(\.\d+)?/
-
-    currency_match = string.match(currency_pattern)
-    price_match = string.match(price_pattern)
-  
-    if currency_match && price_match
-      currency = currency_match[0]
-      price = price_match[0].to_i
+  def extract_price_and_currency(string)
+    match = string.match(/(\D+)\s*([\d\s]+)/)
+    if match
+      currency = match[1].strip
+      price = match[2].strip.gsub(/\s+/, '').to_i
       return price, currency
     else
       return nil, nil
